0158676282d0946265530b307bf0492861a53b5c
Nikhil <nmahalanobis@knights.ucf.edu>
2021-01-27 14:14:58 -0500

    Got pattern matching working correctly. Have 2 different types of matching patterns

--- Seedlings/game/Scenes/VarMinigameIntro.lua
+++ Seedlings/game/Scenes/VarMinigameIntro.lua
@@ -7,31 +7,64 @@ function VarMinigameIntro:new()
     redShirtImage = love.graphics.newImage('Assets/Images/RedShirt-removebg-preview.png')
     yellowShirtImage = love.graphics.newImage('Assets/Images/YellowShirt-removebg-preview.png')
 
-    self.shirtScaleX = 1.15
-    self.shirtScaleY = 1.15
-    
-    self.shirt1 = Shirt('blue', generateRandomShirtNumber())
-    self.shirt2 = Shirt('red', generateRandomShirtNumber())
-    self.shirt3 = Shirt('black', generateRandomShirtNumber())
-    self.shirt4 = Shirt('green', generateRandomShirtNumber())
-    self.shirt5 = Shirt('blue', generateRandomShirtNumber())
-    self.shirt6 = Shirt('yellow', generateRandomShirtNumber())
+    self.shirtScaleX = 0.9
+    self.shirtScaleY = 0.9
 
+    -- Random Pattern selection, choosing between 2 different patterns at the  moment
+    randomSelector = love.math.random(1, 2)
+    
+    -- Generate random shirts
+    self.shirt1 = Shirt(generateRandomShirtColor(), generateRandomShirtNumber())
+    self.shirt2 = Shirt(generateRandomShirtColor(), generateRandomShirtNumber())
+    self.shirt3 = Shirt(generateRandomShirtColor(), generateRandomShirtNumber())
+    self.shirt4 = Shirt(generateRandomShirtColor(), generateRandomShirtNumber())
+    self.shirt5 = Shirt(generateRandomShirtColor(), generateRandomShirtNumber())
+    self.shirt6 = Shirt(generateRandomShirtColor(), generateRandomShirtNumber())
+
+    -- Populate shirt  table w/ randomly generated shirts
     self.shirtTable = {self.shirt1, self.shirt2, self.shirt3, self.shirt4, self.shirt5, self.shirt6}
 
-    self.desiredSequence = {'green', 'blue', 'red', 'green', 'blue', 'red'}
+    -- Depending on what random pattern is chosen, choose a different desiredSequence
+    desiredSequence = {}
+    
+    if randomSelector ==  1 then
+        desiredSequence[1] = 'green'
+        desiredSequence[2] = 'red'
+        desiredSequence[3] = 'blue'
+        desiredSequence[4] = 'green'
+        desiredSequence[5] = 'red'
+        desiredSequence[6] = 'blue'
+    elseif randomSelector == 2 then
+        desiredSequence[1] = 'blue'
+        desiredSequence[2] = 'red'
+        desiredSequence[3] = 'blue'
+        desiredSequence[4] = 'red'
+        desiredSequence[5] = 'blue'
+        desiredSequence[6] = 'red'
+    end
+
+    -- Options for selecting  variables and colors
     self.variablesOptions = {'shirtColor', 'shirtNumber'}
     self.colorOptions = {'red',  'green', 'blue', 'yellow'}
+    
+    -- Populate the user's default sequence w/ the randomly generated shirts
     self.userSequence = {}
+    for i = 1, #self.shirtTable do
+        self.userSequence[i] = self.shirtTable[i].shirtColor
+    end
 
+    -- Selectors for UI
     self.selectedTop = 1
     self.selectedBotLeft = 1
     self.selectedBotRight = 1
     
+    -- Selector flags for choosing variables and values
     self.selectingShirt = true
     self.selectingVariable = false
     self.selectingValues = false
 
+    -- Indicator to see if we've matched the desired sequence
+    self.sequencesMatching = false
 end
 
 function VarMinigameIntro:update()
@@ -67,6 +100,7 @@ function VarMinigameIntro:update()
         if self.selectingValues  then
             self.userSequence[self.selectedTop] = self.colorOptions[self.selectedBotRight]
             self.selectingValues = false
+            self.selectingVariable  = false
         elseif self.selectingShirt and self.selectingVariable and self.selectedBotLeft == 1 then
             self.selectingValues = true
         elseif  self.selectingShirt then
@@ -77,47 +111,81 @@ function VarMinigameIntro:update()
     if inputManager:isPressed('b') then
         if self.selectingShirt and self.selectingVariable then
             self.selectingValues = false
-        elseif self.selectingVariable then
+        end
+        if self.selectingVariable then
             self.selectingVariable = false
         end
     end
     
+    -- Loop w/ counter to check every update if the desired sequence matches the user's  input
+    local counter = 0
+    for i = 1, #desiredSequence  do
+        if desiredSequence[i] == self.userSequence[i] then
+            counter = counter + 1
+        end
+    end
+    if counter == #desiredSequence then
+        self.sequencesMatching = true
+    end
+
     return self
 end
 
 function VarMinigameIntro:drawTopScreen()
    
-    for i =  1, #self.shirtTable do 
-        object = self.shirtTable[i]
-        local shirtColor = object.shirtColor
+    -- Printing  desired sequence 
+    for i = 1, #desiredSequence do
+        local shirtColor =  desiredSequence[i]
+        fontManager:setFont('18px_bold')
+        love.graphics.print('DESIRED SEQUENCE', 105, 25)
 
         if shirtColor == 'red' then
-            love.graphics.draw(redShirtImage, 50 * i, 20, 0, self.shirtScaleX, self.shirtScaleY)
+            love.graphics.draw(redShirtImage, 50 * i, 50, 0, self.shirtScaleX, self.shirtScaleY)
         elseif shirtColor == 'blue' then
-            love.graphics.draw(blueShirtImage, 50 * i, 20, 0, self.shirtScaleX, self.shirtScaleY)
+            love.graphics.draw(blueShirtImage, 50 * i, 50, 0, self.shirtScaleX, self.shirtScaleY)
         elseif shirtColor == 'green' then
-            love.graphics.draw(greenShirtImage, 50 * i, 20, 0, self.shirtScaleX, self.shirtScaleY)
+            love.graphics.draw(greenShirtImage, 50 * i, 50, 0, self.shirtScaleX, self.shirtScaleY)
         elseif shirtColor == 'black' then
-            love.graphics.draw(blackShirtImage, 50 * i, 20, 0, self.shirtScaleX, self.shirtScaleY)
+            love.graphics.draw(blackShirtImage, 50 * i, 50, 0, self.shirtScaleX, self.shirtScaleY)
         elseif shirtColor == 'yellow' then
-            love.graphics.draw(yellowShirtImage, 50 * i, 20, 0, self.shirtScaleX, self.shirtScaleY)
+            love.graphics.draw(yellowShirtImage, 50 * i, 50, 0, self.shirtScaleX, self.shirtScaleY)
+        end 
+
+    end
+    -- Printing "closet" shirts
+    for i =  1, #self.userSequence do 
+        local shirtColor = self.userSequence[i]
+        love.graphics.print('CLOSET SEQUENCE', 100, 140)
+
+        if shirtColor == 'red' then
+            love.graphics.draw(redShirtImage, 50 * i, 160, 0, self.shirtScaleX, self.shirtScaleY)
+        elseif shirtColor == 'blue' then
+            love.graphics.draw(blueShirtImage, 50 * i, 160, 0, self.shirtScaleX, self.shirtScaleY)
+        elseif shirtColor == 'green' then
+            love.graphics.draw(greenShirtImage, 50 * i, 160, 0, self.shirtScaleX, self.shirtScaleY)
+        elseif shirtColor == 'black' then
+            love.graphics.draw(blackShirtImage, 50 * i, 160, 0, self.shirtScaleX, self.shirtScaleY)
+        elseif shirtColor == 'yellow' then
+            love.graphics.draw(yellowShirtImage, 50 * i, 160, 0, self.shirtScaleX, self.shirtScaleY)
         end
 
         if i == self.selectedTop then
-            love.graphics.print('*', 50 * i, 40)
+            love.graphics.print('*', 50 * i, 190)
         end
     end
 
+    --[[
     for i,v in ipairs(self.userSequence) do
-        love.graphics.print(v, 30  * i, 100)
+        love.graphics.print(v, 30  * i, 150)
     end
-    
+    ]]--
 
 end
 
 function VarMinigameIntro:drawBottomScreen()
     
     if self.selectingVariable and self.selectingValues then  
+        --[[
         for i,v in ipairs(self.variablesOptions) do
             love.graphics.print(v, 20, 30 * i)
 
@@ -125,7 +193,7 @@ function VarMinigameIntro:drawBottomScreen()
                 love.graphics.print('*', 10, 30 * i)
             end
         end
-        
+        ]]--
         for i,v in ipairs(self.colorOptions) do 
             love.graphics.print(v, 150, 30 * i)
 
@@ -134,7 +202,9 @@ function VarMinigameIntro:drawBottomScreen()
             end
         end   
     
-    elseif self.selectingVariable then
+    end
+    
+    if self.selectingVariable then
         for i,v in ipairs(self.variablesOptions) do
             love.graphics.print(v, 20, 30 * i)
 
@@ -144,10 +214,15 @@ function VarMinigameIntro:drawBottomScreen()
         end
     end
 
+    if self.sequencesMatching then
+        fontManager:setFont('18px_italic')
+        love.graphics.print('Congrats, you matched the shirt pattern!', 10, 200)
+    end
+
 end
 
 function generateRandomShirtColor()
-    colors = {'red', 'blue', 'green', 'yellow', 'orange', 'white', 'black', 'magenta'}
+    colors = {'red', 'blue', 'green', 'yellow', 'black'}
     randomIndex = love.math.random(1, #colors)
     return colors[randomIndex]
 end
