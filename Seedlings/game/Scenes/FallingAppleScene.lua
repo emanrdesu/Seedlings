38fb9f9bd39be8e17ccd25b6d9a3af8b1b659a46
AuroraGarden <joshuaAG@knights.ucf.edu>
2021-04-23 13:18:39 -0400

    Fixed sandbox bug, modified help screen text
    - There was a bug with the sandbox when going back and forth between the help and regular scene for the tutorial color game. This has been fixed.
    - Changed help scene completion text so that the user knows they need to clear the game while not in help mode

--- Seedlings/game/Scenes/FallingAppleScene.lua
+++ Seedlings/game/Scenes/FallingAppleScene.lua
@@ -109,6 +109,11 @@ function FallingAppleScene:new(isTraining, originalRef)
   self.gameClearTextBoxes = TextBoxList()
   self.gameClearTextBoxes:addText("Congratulations! You caught all of the apples. You are ready to move onto the next minigame now.")
   
+  if isTraining then 
+    self.gameClearTextBoxes = TextBoxList()
+    self.gameClearTextBoxes:addText("Game Complete!\nClear the game while not in help mode to move to the next section")
+  end
+  
   self.gameFailTextBoxes = TextBoxList()
   
   local lock = saveManager:getValue('lock') or 0
